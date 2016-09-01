9477227efd3fa7a5bacdca5e02c6da4a5296dd4f
Josh Vick <Josh Vick>
2016-09-01 14:05:35 -0500

    WriteFile Classes

--- WriteFile.h
+++ WriteFile.h
@@ -6,15 +6,22 @@
 #include <fstream>
 using namespace std;
 
-struct WriteFile
-{
-   ofstream output_file;
-   bool closed;
-};
 
-WriteFile* createWriteFile(const char* file_name);
-void destroyWriteFile(WriteFile* wf);
-void writeLine(WriteFile* wf, String* line);
-void close(WriteFile* wf);
+
+class WriteFile{
+
+private:
+	ofstream output_file;
+	bool closed;
+	const char* file_name;
+
+public:
+	WriteFile();
+	~WriteFile();
+	void writeLine(WriteFile* wf, String* line);	
+	void close(WriteFile* wf);
+	const char* setFileName(const char* text);
+	const char* getFileName();	
+};
 
 #endif
